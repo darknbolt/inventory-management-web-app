bb70d4403cc35771f044aa3facbadc589f599668
DarkNBolt <mgrombiz@gmail.com>
2025-07-18 17:47:54 +0200

    Initial Project

---
+++ src/main/java/vortex/imwp/ImwpApplication.java
@@ -0,0 +1,13 @@
+package vortex.imwp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImwpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImwpApplication.class, args);
+    }
+
+}
