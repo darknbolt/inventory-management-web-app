f6c6fd6cce867a3defa6ba9effc811c336cd578e
DarkNBolt <mgrombiz@gmail.com>
2025-07-23 17:59:03 +0200

    Added: Security Configuration Class

---
+++ src/main/java/vortex/imwp/Config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package vortex.imwp.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {return new BCryptPasswordEncoder();}
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request
+                .requestMatchers("/login", "/css/**", "/js/**", "/fonts/**", "/img/**").permitAll()
+                .anyRequest().authenticated());
+
+        http.formLogin(login -> login
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/", true)
+                .permitAll());
+
+        return http.build();
+    }
+}
